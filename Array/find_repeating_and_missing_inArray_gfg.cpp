2934a25d93169ced897f2a1f729cbb845955e738
Prerna_25 <khushipandey2511@gmail.com>
2022-06-06 15:57:32 +0530

    2

---
+++ Array/find_repeating_and_missing_inArray_gfg.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        int* res = new int[2];
+        vector<int> v(n,0);
+        for(int i=0;i<n;i++){
+            v[arr[i]-1]++;
+        }
+        for(int i=0;i<n;i++){
+            if(v[i]==0) res[1]=i+1;
+            if(v[i]>1) res[0]=i+1;
+        }
+        return res;
+        
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
